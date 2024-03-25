bf65e512ad130f0dbec4b1c47139b2bc1f80c1f6
antiNT <32984684+antiNT2@users.noreply.github.com>
2024-03-25 13:50:45 -0400

    first question

--- src/analyzer/visitors/PrintMachineCodeVisitor.java
+++ src/analyzer/visitors/PrintMachineCodeVisitor.java
@@ -5,7 +5,8 @@ import analyzer.ast.*;
 import java.io.PrintWriter;
 import java.util.*;
 
-public class PrintMachineCodeVisitor implements ParserVisitor {
+public class PrintMachineCodeVisitor implements ParserVisitor
+{
     private PrintWriter m_writer = null;
 
     private int MAX_REGISTERS_COUNT = 256;
@@ -18,7 +19,8 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
 
     private final HashMap<String, String> OPERATIONS = new HashMap<>();
 
-    public PrintMachineCodeVisitor(PrintWriter writer) {
+    public PrintMachineCodeVisitor(PrintWriter writer)
+    {
         m_writer = writer;
 
         OPERATIONS.put("+", "ADD");
@@ -28,12 +30,14 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
     }
 
     @Override
-    public Object visit(SimpleNode node, Object data) {
+    public Object visit(SimpleNode node, Object data)
+    {
         return null;
     }
 
     @Override
-    public Object visit(ASTProgram node, Object data) {
+    public Object visit(ASTProgram node, Object data)
+    {
         node.childrenAccept(this, null);
 
         computeLifeVar();
@@ -45,87 +49,122 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
     }
 
     @Override
-    public Object visit(ASTNumberRegister node, Object data) {
+    public Object visit(ASTNumberRegister node, Object data)
+    {
         MAX_REGISTERS_COUNT = ((ASTIntValue) node.jjtGetChild(0)).getValue();
         return null;
     }
 
     @Override
-    public Object visit(ASTReturnStmt node, Object data) {
-        for (int i = 0; i < node.jjtGetNumChildren(); i++) {
+    public Object visit(ASTReturnStmt node, Object data)
+    {
+        for (int i = 0; i < node.jjtGetNumChildren(); i++)
+        {
             RETURNS.add(((ASTIdentifier) node.jjtGetChild(i)).getValue());
         }
         return null;
     }
 
     @Override
-    public Object visit(ASTBlock node, Object data) {
+    public Object visit(ASTBlock node, Object data)
+    {
         node.childrenAccept(this, null);
         return null;
     }
 
     @Override
-    public Object visit(ASTStmt node, Object data) {
+    public Object visit(ASTStmt node, Object data)
+    {
         node.childrenAccept(this, null);
         return null;
     }
 
     @Override
-    public Object visit(ASTAssignStmt node, Object data) {
+    public Object visit(ASTAssignStmt node, Object data)
+    {
         // TODO (ex1): Modify CODE to add the needed MachLine.
         // Here the type of Assignment is "assigned = left op right".
         // You can pass null as data to children.
 
+        String assign = (String) node.jjtGetChild(0).jjtAccept(this, null);
+        String left = (String) node.jjtGetChild(1).jjtAccept(this, null);
+        String right = (String) node.jjtGetChild(2).jjtAccept(this, null);
+        String op = node.getOp();
+
+        this.CODE.add(new MachineCodeLine(op, assign, left, right));
+
         return null;
     }
 
     @Override
-    public Object visit(ASTAssignUnaryStmt node, Object data) {
+    public Object visit(ASTAssignUnaryStmt node, Object data)
+    {
         // TODO (ex1): Modify CODE to add the needed MachLine.
         // Here the type of Assignment is "assigned = - right".
         // Suppose the left part to be the constant "#O".
         // You can pass null as data to children.
 
+        String assign = (String) node.jjtGetChild(0).jjtAccept(this, null);
+        String left = "#0";
+        String right = (String) node.jjtGetChild(1).jjtAccept(this, null);
+        String op = "-";
+
+        this.CODE.add(new MachineCodeLine(op, assign, left, right));
+
         return null;
     }
 
     @Override
-    public Object visit(ASTAssignDirectStmt node, Object data) {
+    public Object visit(ASTAssignDirectStmt node, Object data)
+    {
         // TODO (ex1): Modify CODE to add the needed MachLine.
         // Here the type of Assignment is "assigned = right".
         // Suppose the left part to be the constant "#O".
         // You can pass null as data to children.
 
+        String assign = (String) node.jjtGetChild(0).jjtAccept(this, null);
+        String left = "#0";
+        String right = (String) node.jjtGetChild(1).jjtAccept(this, null);
+        String op = "";
+
+        this.CODE.add(new MachineCodeLine(op, assign, left, right));
+
         return null;
     }
 
     @Override
-    public Object visit(ASTExpr node, Object data) {
+    public Object visit(ASTExpr node, Object data)
+    {
         return node.jjtGetChild(0).jjtAccept(this, null);
     }
 
     @Override
-    public Object visit(ASTIntValue node, Object data) {
+    public Object visit(ASTIntValue node, Object data)
+    {
         return "#" + node.getValue();
     }
 
     @Override
-    public Object visit(ASTIdentifier node, Object data) {
+    public Object visit(ASTIdentifier node, Object data)
+    {
         return node.getValue();
     }
 
-    private void computeLifeVar() {
+    private void computeLifeVar()
+    {
         // TODO (ex2): Implement life variables algorithm on the CODE array.
     }
 
-    private void computeNextUse() {
+    private void computeNextUse()
+    {
         // TODO (ex3): Implement next-use algorithm on the CODE array.
     }
 
     /**
      * This function should generate the LD and ST when needed.
      */
-    public String chooseRegister(String variable, HashSet<String> life, NextUse next, boolean loadIfNotFound) {
+    public String chooseRegister(String variable, HashSet<String> life, NextUse next, boolean loadIfNotFound)
+    {
         // TODO (ex4): if variable is a constant (starts with '#'), return variable
         // TODO (ex4): if REGISTERS contains variable, return "R" + index
         // TODO (ex4): if REGISTERS size is not max (< MAX_REGISTERS_COUNT), add variable to REGISTERS and return "R" + index
@@ -140,10 +179,12 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
     /**
      * Print the machine code in the output file
      */
-    public void printMachineCode() {
+    public void printMachineCode()
+    {
         // TODO (ex4): Print the machine code in the output file.
         // You should change the code below.
-        for (int i = 0; i < CODE.size(); i++) {
+        for (int i = 0; i < CODE.size(); i++)
+        {
             m_writer.println("// Step " + i);
             m_writer.println(CODE.get(i));
         }
@@ -155,7 +196,8 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
      * @param set The set to order
      * @return The ordered list
      */
-    public List<String> orderedSet(Set<String> set) {
+    public List<String> orderedSet(Set<String> set)
+    {
         List<String> list = new ArrayList<>(set);
         Collections.sort(list);
         return list;
@@ -164,29 +206,38 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
     /**
      * A class to store and manage next uses.
      */
-    private class NextUse {
+    private class NextUse
+    {
         public HashMap<String, ArrayList<Integer>> nextUse = new HashMap<>();
 
-        public NextUse() {}
+        public NextUse()
+        {
+        }
 
-        public NextUse(HashMap<String, ArrayList<Integer>> nextUse) {
+        public NextUse(HashMap<String, ArrayList<Integer>> nextUse)
+        {
             this.nextUse = nextUse;
         }
 
-        public ArrayList<Integer> get(String s) {
+        public ArrayList<Integer> get(String s)
+        {
             return nextUse.get(s);
         }
 
-        public void add(String s, int i) {
-            if (!nextUse.containsKey(s)) {
+        public void add(String s, int i)
+        {
+            if (!nextUse.containsKey(s))
+            {
                 nextUse.put(s, new ArrayList<>());
             }
             nextUse.get(s).add(i);
         }
 
-        public String toString() {
+        public String toString()
+        {
             ArrayList<String> items = new ArrayList<>();
-            for (String key : orderedSet(nextUse.keySet())) {
+            for (String key : orderedSet(nextUse.keySet()))
+            {
                 Collections.sort(nextUse.get(key));
                 items.add(String.format("%s:%s", key, nextUse.get(key)));
             }
@@ -194,7 +245,8 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
         }
 
         @Override
-        public Object clone() {
+        public Object clone()
+        {
             return new NextUse((HashMap<String, ArrayList<Integer>>) nextUse.clone());
         }
     }
@@ -202,7 +254,8 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
     /**
      * A struct to store the data of a machine code line.
      */
-    private class MachineCodeLine {
+    private class MachineCodeLine
+    {
         String OPERATION;
         String ASSIGN;
         String LEFT;
@@ -217,7 +270,8 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
         public NextUse Next_IN = new NextUse();
         public NextUse Next_OUT = new NextUse();
 
-        public MachineCodeLine(String operation, String assign, String left, String right) {
+        public MachineCodeLine(String operation, String assign, String left, String right)
+        {
             this.OPERATION = OPERATIONS.get(operation);
             this.ASSIGN = assign;
             this.LEFT = left;
@@ -231,7 +285,8 @@ public class PrintMachineCodeVisitor implements ParserVisitor {
         }
 
         @Override
-        public String toString() {
+        public String toString()
+        {
             String buffer = "";
             buffer += String.format("// Life_IN  : %s\n", Life_IN);
             buffer += String.format("// Life_OUT : %s\n", Life_OUT);
